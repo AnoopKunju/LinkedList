f8ec2c93f8569e3568e66c1fefea4ca89ee25f95
AnoopKunju <anoopkunju@gmail.com>
2017-08-09 15:11:25 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -35,7 +35,7 @@ void append() //function to append a element at the end of linked list
   }
 }
 //----------------------------------------------------------
-int lenght() // counting the number of elements in the list
+int length() // counting the number of elements in the list
 {
  int count=0;
  struct node* temp;
@@ -66,6 +66,39 @@ void addatbegin()
    }
 }
 //----------------------------------------------------------
+void delete()
+{
+ struct node* temp;
+ int loc;
+ printf("Enter location to delete:\n");
+ scanf("%d",&loc);
+  if(loc>length())
+  {
+   printf("INVALID LOCATION \n \n");
+  }
+  else if (loc==1)
+  {
+   temp=root;
+   root=temp->link; //assigning the nodes link into the node which we want to enter
+   temp->link=NULL;
+   free(temp);
+  }
+  else
+  {
+   struct node* p=root,*q;
+   int i=1;
+   while(i<loc-1)
+   {
+     p=p->link;
+     i++;
+   }
+   q=p->link;
+   p->link=q->link;
+   q->link=NULL;
+   free(q);
+  }
+}
+//----------------------------------------------------------
 void display()//for printing the final Linklist
 {
     struct node *temp;
@@ -76,7 +109,7 @@ void display()//for printing the final Linklist
     }
     while(temp!=NULL)
     {
-    printf("%d-->",temp->data);
+    printf("%d -->",temp->data);
     temp=temp->link;
     }
     printf("\n\n");
@@ -86,18 +119,17 @@ void addatPosition()
 {
  struct node* temp,*p;
  int loc,len,locat,i=1;
- printf("Enter location:");
- scanf("%d",&locat);
  temp=(struct node*)malloc(sizeof(struct node));
      printf("enter node data:");
      scanf("%d", &temp->data);
      temp->link=NULL;
+ printf("Enter location:");
+ scanf("%d",&locat);
  loc=locat-1;
- len=lenght();
+ len=length();
   if(loc>len)
     {
      printf("Invalid location \n");
-     printf("currently list is having %d nodes",len);
     }
   else
     {
@@ -114,43 +146,62 @@ void addatPosition()
 //----------------------------------------------------------
 int main()
 {
- int a,b,i,c;
- printf("Welcome to Linked List \n");
+ int a,b,i,c,d;
+ printf("\n||_____Welcome to Linked List_____||\n\n");
  printf("How many elements you wish to enter: \n");
  scanf("%d",&b);  //taking elements for the linked list
-  for(i=1;i<=b;i++)
+  for(i=1;i<=b;i++)//reads data for the limit entered
    {
      append();  //entering the elements at the end of list
+     display();
    }
- printf("Do you wanna enter more elements? Yes(1)/No(0)");
- scanf("%d",&a);
-   if(a==1)
+ printf("\n-------Operations on Linked List-------\n\n"); 
+ while(1)
+ {
+  printf("1.INSERT more elements \n");
+  printf("2.DELETE elements \n");
+  printf("3.SORT elements \n");
+  printf("4.DISPLAY the Linked List \n");
+  printf("5.EXIT\n");
+  scanf("%d",&a);
+   switch(a)
     {
-     // append(); for entering a new element at the end/into the list
-      printf("Where would you like to enter the element in the Linked List: \n");
-      printf("1.At the Begining \n");
-      printf("2.At the End \n");
-      printf("3.At the specific Position \n");
-      scanf("%d",&c);
-      switch(c)
-      {
-      case 1: addatbegin();
-              display();
-              break;
-      case 2: append();
-              display();
-              break;
-      case 3: addatPosition();
-              display();
-              break;  
-      }
+    case 1: printf("Where would you like to enter the element in the Linked List: \n");
+            printf("1.At the Begining \n");
+            printf("2.At the End \n");
+            printf("3.At the specific Position \n");
+            scanf("%d",&c);
+            switch(c)
+             {
+               case 1: addatbegin();
+                       display();
+                       break;
+               case 2: append();
+                       display();
+                       break;
+               case 3: addatPosition();
+                       display();
+                       break;
+             }
+            break;
+    case 2: printf("Delected List:\n");
+            delete();
+            break;
+   // case 3:
+    case 4: printf("Linked list: \n\n");
+            display();
+            break;
+    case 5: printf("Thank you for using LINKED LIST\n");
+            printf("Confirm you wanna EXIT?\n yes(1)/no(0)");
+            scanf("%d",&d);
+            if(d==1)
+            {
+             exit(0);
+            }
+            break;
     }
-   else
-    {
-      printf("Thankyou for using Linked list \n");
-      printf("Linked list: \n");
-      display();
-    }
+ }
+  
 }
 
  
